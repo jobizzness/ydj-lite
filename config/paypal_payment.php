4c28dbeccf7eee57f410ce3dd0b78c9ac6617abb
unknown <jobizzness@github.io>
2018-01-22 00:16:52 +0800

    paypal is live

--- config/paypal_payment.php
+++ config/paypal_payment.php
@@ -2,7 +2,7 @@
 
 return [
     'mode' => 'sandbox',
-    'service.EndPoint' => 'https://api.sandbox.paypal.com', // <- change later to production
+    'service.EndPoint' => 'https://api.paypal.com', // <- change later to production
     'http.ConnectionTimeOut' => 30,
     'log.LogEnabled' => true,
     'log.FileName' => storage_path('logs/paypal.log'), // <- Logging here
